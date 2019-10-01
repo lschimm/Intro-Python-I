fbcb371abe079258c7385e2bfef73b15c58328e8
lschimm <lschimmelfennig@gmail.com>
2019-10-01 15:12:06 -0500

    Adds to functions section. Will finish

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,35 @@
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    return num % 2 == 0
+
+
+is_even(11)
+
+# try this...
+
+
+def is_even(num):
+    if (num % 2 == 0)
+    print("True!")
+    else:
+        print("False! Boooooo")
+
+
+is_even(20)
+is_even(21)
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
+if is_even(num):
+    print("Even!")
+else:
+    print("Odd!")
 
+is_even(20)
--- src/tuesday-lecture.py
+++ src/tuesday-lecture.py
@@ -6,3 +6,29 @@ a = (1, 2, 3)
 a[0] = 20  # CAN'T DO
 
 a[0]
+
+# Passing by value - interger or string
+
+
+def foo(x):
+    x = 10
+
+
+a = 20
+
+foo(a)
+
+print(a)
+
+# Passing by reference - tuple, lists, dictionaries, objects etc
+
+
+def bar(x):
+    x.append(99)
+
+
+a = [1, 2, 3]
+
+bar(a)
+
+print(a)
