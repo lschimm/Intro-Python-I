7af0803e5da05873498c3f7c42923fb30b0bb8e4
lschimm <lschimmelfennig@gmail.com>
2019-10-01 17:33:40 -0500

    Fixes typos, etc

--- src/03_modules.py
+++ src/03_modules.py
@@ -15,22 +15,22 @@ import sys
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print('OS Platform: ', sys.platform)
+print('OS Platform:', sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print('Python Version: ', sys.version)
+print('Python Version:', sys.version)
 
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-print('Current Process ID: ', os.getpid())
+print('Current Process ID:', os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print('Current Working Directory: ', os.getcwd())
+print('Current Working Directory:', os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
-print("Machine's Current Login Name: ", os.getlogin())
+print('Machine's Current Login Name: ', os.getlogin())
