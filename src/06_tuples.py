69c3e9f865f829fb5160e4275aa7d385a6dbd235
lschimm <lschimmelfennig@gmail.com>
2019-10-29 12:40:04 -0500

    Tidies up 6

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -34,6 +34,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
+def print_tuple(t):
+    print(f"{t}")
+
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
